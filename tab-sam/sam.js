abdf569141e868c531fc5e9fd618fcff30a4f53c
funfunido <161741261+funfunido@users.noreply.github.com>
2025-02-14 10:25:53 -0700

    Update sam.js

--- tab-sam/sam.js
+++ tab-sam/sam.js
@@ -3,6 +3,7 @@ import {samjs} from './samjs';
 let sam = new samjs.SamJs();
 
 chrome.commands.onCommand.addListener(function (command) {
+   alert("it works")
     if (command === "talk") {
         sam.speak(document.activeElement.innerHTML);
     }
