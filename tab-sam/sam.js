cd4d3d66c53f50b2d62c3336efa1cc66ece740c8
Teker <ellieputnam5@gmail.com>
2025-02-14 17:14:24 +0000

    arf aughaoihga0our90hehtoai arf ruff mmmmmmarf!!!!!!

--- tab-sam/sam.js
+++ tab-sam/sam.js
@@ -1,1 +1,9 @@
-importScripts("")
+const samjs = require("./samjs")
+
+let sam = new samjs.SamJs();
+
+chrome.commands.onCommand.addListener(function (command) {
+    if (command === "talk") {
+        sam.speak(document.activeElement.innerHTML);
+    }
+});
