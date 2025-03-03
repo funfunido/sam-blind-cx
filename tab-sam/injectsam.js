bcfb09c15326c001caa3b60614e8942b90778d75
funfunido <161741261+funfunido@users.noreply.github.com>
2025-03-03 09:55:48 -0700

    d

--- tab-sam/injectsam.js
+++ tab-sam/injectsam.js
@@ -31,32 +31,7 @@ console.log("Sam Tab Extension Invoked");
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Tab') {
-        setTimeout(function() {
-            console.log("Tab Key Pressed");
-            const textToSpeak = document.activeElement.value || document.activeElement.innerText || '';
-            let fullApiLink = (firstPart + encodeURIComponent(textToSpeak) + secondPart);
-            const ctx = new AudioContext();
-            let audio;
-
-            fetch(fullApiLink, { mode: 'no-cors' })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.arrayBuffer();
-                })
-                .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
-                .then(decodedAudio => {
-                    audio = decodedAudio;
-                    const playSound = ctx.createBufferSource();
-                    playSound.buffer = audio;
-                    playSound.connect(ctx.destination);
-                    playSound.start(ctx.currentTime);
-                })
-                .catch(error => {
-                    console.error('Error fetching or decoding audio:', error);
-                });
-
-        }, 1000);
+        alert("Tab Pressed");
+        console.log("Tab Pressed");
     }
 });
